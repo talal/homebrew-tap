e9776fb1ee7b4d757cda9bf90dcdd7a9dc0d1aca
Muhammad Talal Anwar <talalanwar@outlook.com>
2019-01-23 23:24:22 +0100

    mimir: update to v1.2.0

--- Formula/mimir.rb
+++ Formula/mimir.rb
@@ -1,11 +1,11 @@
 class Mimir < Formula
-	desc "A fast and minimal prompt for Bash and Zsh"
-	homepage "https://github.com/talal/mimir"
-	url "https://github.com/talal/mimir/releases/download/v1.1.2/mimir-1.1.2-darwin_amd64.tar.gz"
-	version "1.1.2"
-	sha256 "5127f4023f435b6c215bafb0cf84d7b2d9c4377f87189c09b2f7f1d6c1d4091c"
+  desc "A fast and minimal prompt for Bash and Zsh"
+  homepage "https://github.com/talal/mimir"
+  url "https://github.com/talal/mimir/releases/download/v1.2.0/mimir-1.2.0-darwin_amd64.tar.gz"
+  version "1.2.0"
+  sha256 "d9f1dd1277ce28c44eca1ebb65f5a5eeb91e49b6bfd43ff49867b8934979b165"
 
-	def install
-		bin.install "mimir"
-	end
+  def install
+    bin.install "mimir"
+  end
 end
