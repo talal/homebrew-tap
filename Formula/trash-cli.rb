96716648c5f80d47b2dc84e0ee2f166bd6b01ebe
Muhammad Talal Anwar <mail@talal.io>
2022-04-06 23:53:00 +0200

    Add trash-cli

---
+++ Formula/trash-cli.rb
@@ -0,0 +1,27 @@
+require "language/node"
+
+class TrashCli < Formula
+  desc "Move files and folders to the trash"
+  homepage "https://github.com/sindresorhus/trash-cli"
+  url "https://github.com/sindresorhus/trash-cli/archive/refs/tags/v5.0.0.tar.gz"
+  sha256 "a5ba8ddc1a9a670c2af5e679e0c49cc56779032996842e2cd137a2529d6c1047"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "node" => :build
+
+  def install
+    Language::Node.setup_npm_environment
+
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_equal version.to_s, shell_output("#{bin}/trash --version").chomp
+  end
+end
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # homebrew-tap
 
+This repository contains Homebrew formulae for my projects and tools that I use which are
+not available in the Homebrew core tap.
+
 ## Installation
 
 ```
@@ -12,5 +15,10 @@ brew tap talal/homebrew-tap
 brew install <formula-name>
 ```
 
-[bonclay]: https://github.com/talal/bonclay
-[mimir]: https://github.com/talal/mimir
+## Formulae
+
+| Name | Description |
+| --- | --- |
+| trash-cli | [trash-cli] is a tool for moving files and folders to the trash. |
+
+[trash-cli]: https://github.com/sindresorhus/trash-cli
