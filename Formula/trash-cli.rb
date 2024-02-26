b28ad686116307ec60af93fd2171a0c8f5e18e09
Muhammad Talal Anwar <muhammad.talal.anwar@sap.com>
2024-02-26 07:44:06 +0100

    trash-cli: update node to v20

--- Formula/trash-cli.rb
+++ Formula/trash-cli.rb
@@ -6,20 +6,20 @@ class TrashCli < Formula
   url "https://github.com/sindresorhus/trash-cli/archive/refs/tags/v5.0.0.tar.gz"
   sha256 "a5ba8ddc1a9a670c2af5e679e0c49cc56779032996842e2cd137a2529d6c1047"
   license "MIT"
-  revision 2
+  revision 3
 
   livecheck do
     url :stable
     strategy :github_latest
   end
 
-  depends_on "node@16"
+  depends_on "node@20"
 
   def install
     Language::Node.setup_npm_environment
 
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    (bin/"trash").write_env_script libexec/"bin/trash", PATH: "#{Formula["node@16"].opt_bin}:$PATH"
+    (bin/"trash").write_env_script libexec/"bin/trash", PATH: "#{Formula["node@20"].opt_bin}:$PATH"
   end
 
   test do
